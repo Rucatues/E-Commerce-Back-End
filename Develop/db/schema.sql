0b19e86ec7b9101dceac87cdd5cdb6445f5b82c1
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-11 13:30:01 -0600

    removed comment from schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -3,13 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
--- You'll need to execute association methods on your Sequelize models to create the following relationships between them:
-
-        --  Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
-
-        -- Category has many Product models.
-
-        -- Product belongs to many Tag models. Using the ProductTag through model, allow products to have multiple tags and tags to have many products.
-
-        -- Tag belongs to many Product models.
