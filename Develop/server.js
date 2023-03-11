90fbdf199e745a3cb34830b9e5ff47b936f61292
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-11 13:13:58 -0600

    added sequelize sync to app.listen

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,6 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 
-// sequelize.sync().then(() => {
-app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
-// });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
+});
